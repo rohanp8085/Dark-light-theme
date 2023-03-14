d206c8fa1d95c8d7e52a2eb1816808a5130f811e
unknown <rohanprajapati3739@gmail.com>
2023-03-14 16:16:02 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const toggle = document.getElementById("toggledark")
+const body = document.querySelector("body")
+
+toggle.addEventListener('click',function(){
+    this.classList.toggle('bi-moon')
+    if (this.classList.toggle('bi-brightness-high-fill')) {
+        body.style.backgroundColor = "white"
+        body.style.color = "black"
+        body.style.transition = "2s"
+    }
+    else{
+        body.style.backgroundColor = "black"
+        body.style.color = "white"
+        body.style.transition = "2s"
+
+    }
+})
